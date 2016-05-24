5efc384160a20a42bfe8bc73e2e67593bde27dad
Shaws <seanrichardparker@gmail.com>
2016-05-24 14:47:48 -0600

    Sean's changes

--- blog_post/config/routes.rb
+++ blog_post/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/#home'
+  get '/about', to: 'static_pages#about', as: 'about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
